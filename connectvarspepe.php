2dc458145ec5c0cef45677113eca841e72efea64
jcv33a <jcv33a@gmail.com>
2025-03-09 18:57:20 +0100

    Update connectvarspepe.php

--- connectvarspepe.php
+++ connectvarspepe.php
@@ -2,6 +2,6 @@
   // Define database connection constants
    define('DB_HOST', 'db555808602.db.1and1.com');
    define('DB_USER', 'dbo555808602');
-   define('DB_PASSWORD', 'josecruzvela');
+   define('DB_PASSWORD', '');
    define('DB_NAME', 'db555808602');
 ?>
